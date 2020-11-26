3b64383bf7573394af326943094e92fcec4388ff
ftomi <tomifarkas@yahoo.com>
2020-11-26 08:47:42 +0100

    Design fixek

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
-
+  pinkBorder = [false, false, false, false, false, false, false, false];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setBorder(idx: number): void {
+    const prevState = this.pinkBorder[idx];
+    this.pinkBorder = [false, false, false, false, false, false, false, false];
+    this.pinkBorder[idx] = !prevState;
+  }
 }
